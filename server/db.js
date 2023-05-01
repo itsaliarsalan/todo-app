0d48c6e290e8cab19b910c09138a0f3ecdffbe16
Arsalan Ali <101986616+aliarsalandev@users.noreply.github.com>
2023-05-01 17:51:08 +0500

    updated connection url

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   password: process.env.PASSWORD,
   host: process.env.HOST,
   port: process.env.DBPORT,
-  database: 'todosdb'
+  database: process.env.DATABASE,
 })
 
 module.exports = pool
